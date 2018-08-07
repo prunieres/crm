fc15c91139476ee59af7a45329d8c3509479ae40
PRUNIERES <aurelie.prunieres@gmail.com>
2018-08-07 10:19:52 +0200

    structure nav and tab pan

--- index.php
+++ index.php
@@ -11,7 +11,37 @@
 </head>
 
 <body>
-<h1>Notre super CRM</h1>
+    <nav class="navbar navbar-light bg-light">
+        <h1>Notre super CRM</h1>
+        <ul>
+            <li><a href="">Listing</a></li>
+            <li><a href="">Ajouter clients</a></li>
+            <li><a href="">Ajouter entreprises</a></li>
+        </ul>
+    </nav>
+    <h2>Listing clients/Entreprises</h2>
+    <div class="container">
+        <ul class="nav nav-tabs">
+            <li class="active"><a data-toggle="tab" href="#home">Clients(3)</a></li>
+            <li><a data-toggle="tab" href="#menu1">Entreprises(4)</a></li>
+        </ul>
+
+      <div class="tab-content">
+          <div id="home" class="tab-pane fade in active">
+              <div class="input-group mb-3">
+                  <input type="text" class="form-control" placeholder="Recherche" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                  <button>Search</button>
+                  <button>Reset</button>
+              </div>
+          </div>
+
+        <div id="menu1" class="tab-pane fade">
+          <h3>Entreprises(4)</h3>
+          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+
+      </div>
+</div>
     <script src="node_modules/jquery/dist/jquery.min.js" crossorigin="anonymous"></script>
     <script src="node_modules/bootstrap/dist/js/bootstrap.js" crossorigin="anonymous"></script>
     <script src="script.js"></script>
